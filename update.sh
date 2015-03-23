c7e327b666bbf301614338f7d46008bae8374e83
Moghedrin <nolat301@gmail.com>
2015-03-23 14:34:12 -0600

    Update sha256 in update script

--- update.sh
+++ update.sh
@@ -9,7 +9,10 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+curl -s "https://www.ruby-lang.org/en/downloads/" | tr -d "\r\n\t" > shafile.html
+
 for version in "${versions[@]}"; do
+	shaVal="$(sed -r "s/^.*Ruby ${fullVersion}.*sha256: ([^<]+).*/\1/" shafile.html)"
 	fullVersion="$(curl -sSL --compressed "http://cache.ruby-lang.org/pub/ruby/$version/" \
 		| grep -E '<a href="ruby-'"$version"'.[^"]+\.tar\.bz2' \
 		| grep -vE 'preview|rc' \
@@ -19,6 +22,7 @@ for version in "${versions[@]}"; do
 		set -x
 		sed -ri 's/^(ENV RUBY_MAJOR) .*/\1 '"$version"'/' "$version/"{,wheezy/,slim/}Dockerfile
 		sed -ri 's/^(ENV RUBY_VERSION) .*/\1 '"$fullVersion"'/' "$version/"{,wheezy/,slim/}Dockerfile
+		sed -ri 's/^(ENV RUBY_DOWNLOAD_SHA256) .*/\1 '"$shaVal"'/' "$version/"{,wheezy/,slim/}Dockerfile
 		sed -ri 's/^(FROM ruby):.*/\1:'"$fullVersion"'/' "$version/"*"/Dockerfile"
 	)
 done
