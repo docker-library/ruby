abad497073efcd2537bb0a106c3515127bbccba4
Tianon Gravi <admwiggin@gmail.com>
2016-02-03 12:11:16 -0800

    Templatize our Dockerfiles

--- update.sh
+++ update.sh
@@ -25,22 +25,21 @@ for version in "${versions[@]}"; do
 		| sed -r 's!.*<a href="ruby-([^"]+)\.tar\.bz2.*!\1!' \
 		| sort -V | tail -1)"
 	shaVal="$(echo $shaPage | sed -r "s/.*Ruby ${fullVersion}<\/a><br \/> sha256: ([^<]+).*/\1/")"
-	(
-		set -x
-		sed -ri '
-			s/^(ENV RUBY_MAJOR) .*/\1 '"$version"'/;
-			s/^(ENV RUBY_VERSION) .*/\1 '"$fullVersion"'/;
-			s/^(ENV RUBY_DOWNLOAD_SHA256) .*/\1 '"$shaVal"'/;
-			s/^(ENV BUNDLER_VERSION) .*/\1 '"$bundler"'/;
-			s/^(ENV RUBYGEMS_VERSION) .*/\1 '"$rubygems"'/;
-		' "$version"{/,/*/}Dockerfile
-		sed -ri 's/^(FROM ruby):.*/\1:'"$version"'/' "$version/"*"/Dockerfile"
-	)
-	for variant in alpine slim; do
+
+	sedStr="
+		s!%%VERSION%%!$version!g;
+		s!%%FULL_VERSION%%!$fullVersion!g;
+		s!%%SHA256%%!$shaVal!g;
+		s!%%RUBYGEMS%%!$rubygems!g;
+		s!%%BUNDLER%%!$bundler!g;
+	"
+	for variant in alpine slim onbuild ''; do
 		[ -d "$version/$variant" ] || continue
-		travisEnv='\n  - VERSION='"$version VARIANT=$variant$travisEnv"
+		sed -r "$sedStr" "Dockerfile${variant:+-$variant}.template" > "$version/$variant/Dockerfile"
+		if [ "$variant" != 'onbuild' ]; then
+			travisEnv='\n  - VERSION='"$version VARIANT=$variant$travisEnv"
+		fi
 	done
-	travisEnv='\n  - VERSION='"$version VARIANT=$travisEnv"
 done
 
 travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
