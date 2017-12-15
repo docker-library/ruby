e2675d27fcf0f0212c470b17001cb3890a109fd6
Callum Jones <callum@hoteltonight.com>
2017-12-15 13:23:08 -0800

    more

--- build.rb
+++ build.rb
@@ -4,11 +4,24 @@ NAME = "callumj/ruby-jemalloc"
 
 require 'open3'
 
+def run_command(cmd)
+  Open3.popen3(cmd) do |stdout, stderr, status, thread|
+    Thread.new do
+      while line = stdout.gets do
+        puts(line)
+      end
+    end
+    while line = stderr.gets do
+      puts(line)
+    end
+  end
+end
+
 path = File.expand_path(File.dirname(__FILE__))
 VERSIONS_TO_BUILD.each do |version|
   dockerfiles = Dir.glob("#{path}/#{version}/**/Dockerfile")
   dockerfiles.each do |path|
-    next if path.match(/onbuild/)
+    next if path.match(/(onbuild|alpine)/)
 
     contents = File.read(path)
     tag = TAG_TO_FIND.match(contents)
@@ -17,16 +30,8 @@ VERSIONS_TO_BUILD.each do |version|
     final_tag = "#{full_version}-#{base}"
     image_name = "#{NAME}:#{final_tag}"
     Dir.chdir(File.dirname(path)) do
-      Open3.popen3("docker build -t #{image_name} .") do |stdout, stderr, status, thread|
-        Thread.new do
-          while line = stdout.gets do 
-            puts(line) 
-          end
-        end
-        while line = stderr.gets do 
-          puts(line) 
-        end
-      end
+      run_command("docker build -t #{image_name} .")
+      run_command("docker push #{image_name}")
     end
   end
 end
